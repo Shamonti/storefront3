35c4da4e8866047f7b1ddbe3b7c93faec139fa4f
Shamonti <shamonti.haque98@gmail.com>
2026-04-08 12:53:14 +0600

    configured logging

--- playground/views.py
+++ playground/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
+import logging
 import requests
 
 # def say_hello(request):
@@ -14,14 +15,19 @@ import requests
 
 #     return render(request, 'hello.html', {'name': cache.get(key)})
 
+logger = logging.getLogger(__name__)
 
 class HelloView(APIView):
     @method_decorator(cache_page(5 * 60))
     def get(self, request):
-        response = requests.get('https://httpbin.org/delay/2')
-        data = response.json()
+        try:
+            logger.info('Calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info('Response received')
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical('httpbin is offline')
         return render(request, 'hello.html', {'name': 'Shamonti'})
-
 
 # @cache_page(5 * 60)
 # def say_hello(request):
--- storefront/settings.py
+++ storefront/settings.py
@@ -61,10 +61,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -199,6 +199,35 @@ CACHES = {
         "TIMEOUT": 10 * 60,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'general.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': os.environ.get('DJANGO_LOG_LEVEL', 'INFO')
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '{asctime} ({levelname}) - {name} - {message} ',
+            'style': '{',
         },
     }
 }
